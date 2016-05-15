be97bf9fe26b1c4faf83e621c00bf3f2641c92ad
atnartur <i@atnartur.ru>
2016-05-15 23:01:40 +0300

    + console.log

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 */
 
 $(document).ready(function(){
+	console.log('reshuege ext init')
 	setTimeout(function(){
 		$('body div[style="margin:0;padding:0; bottom:10px; right:10px; position:fixed; border:none; z-index:1"]').detach();
 	}, 2000);

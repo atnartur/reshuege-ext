2799c12868b86c114d5a96fc16a70baa1b242dc1
atnartur <i@atnartur.ru>
2016-05-15 22:52:17 +0300

    + code

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@
 * @Author: Artur Atnagulov (ClienDDev team)
 */
 
+$(document).ready(function(){
+	setTimeout(function(){
+		$('body div[style="margin:0;padding:0; bottom:10px; right:10px; position:fixed; border:none; z-index:1"]').detach();
+	}, 2000);
+});
